dd7fcd077589013fc10a35b52a90a1606a58f00e
Maurizio <proiettomaurizio97@gmail.com>
2022-01-31 15:38:20 +0100

    front end

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,6 +1,41 @@
 @extends('layouts.main-layout')
 @section('content')
-    <h1>List Film:</h1>
+
+    <nav>
+        <div>
+            <a href="">
+                <h2>
+                    Subscribe
+                </h2>
+            </a>
+        </div>
+        <div>
+            <a href="">
+                <h2>
+                    Sign in
+                </h2>
+            </a>
+        </div>
+    </nav>
+    <section>
+        <h1>List User</h1>
+        <ul>
+            @foreach ($users as $user)
+                <li>
+                    <span>
+                        <a href="">
+                            Utente n.{{ $user -> id }}: <br>
+                        </a>
+                    </span>
+                    <span>- Nome:</span> <span class="val">{{ $user -> name }}</span><br>
+                    <span>- Cognome:</span> <span class="val">{{ $user -> lastname }}</span><br>
+                    <span>- User Name:</span> <span class="val">{{ $user -> user_name }}</span><br>
+                </li>
+            @endforeach
+        </ul>
+    </section>
+
+    {{-- <h1>List Film:</h1>
 
     <a href="{{ route('create') }}">
         CREATE NEW FILM
@@ -16,5 +51,5 @@
             - <a href="{{ route('delete', $film -> id) }}">DELETE</a>
         </li>
     @endforeach
-    </ul>
+    </ul> --}}
 @endsection
