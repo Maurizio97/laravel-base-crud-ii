28ae5ba042ad2eb9826dc8cd32a3daa0ddcdbe53
Maurizio <proiettomaurizio97@gmail.com>
2022-01-31 15:11:11 +0100

    create and set seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,10 +2,10 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Model;
+use App\User;
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(User::class, function (Faker $faker) {
     return [
             'name' => $faker -> firstName(),
             'lastname' => $faker -> lastName(),
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            FilmSeeder::class
+            FilmSeeder::class,
+            UserSeeder::class,
         ]);
     }
 }
