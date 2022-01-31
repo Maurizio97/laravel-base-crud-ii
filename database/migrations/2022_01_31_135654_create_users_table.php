d92c31d97c8afd265d6524ba07ec0a8c526585ca
Maurizio <proiettomaurizio97@gmail.com>
2022-01-31 15:00:18 +0100

    create and set migration users

--- database/migrations/2022_01_31_135654_create_users_table.php
+++ database/migrations/2022_01_31_135654_create_users_table.php
@@ -14,9 +14,15 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
+            $table -> id();
+            $table -> string('name', 30);
+            $table -> string('lastname', 30);
+            $table -> string('user_name', 30);
+            $table -> string('email', 50);
+            $table -> string('password', 30);
+
+            $table -> timestamps();
+        }); 
     }
 
     /**
